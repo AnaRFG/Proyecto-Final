67dc31cc07957f8f364ac9d8d6187b29dc48b29e
jazminmorenasanchez <2321544@ucc.edu.ar>
2024-06-12 11:18:36 -0300

    agregue cositas al main

--- NEWS.cpp
+++ NEWS.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
-#include "Persona.h"
-#include "autor.h"
+//#include "Persona.h"
+//#include "autor.h"
 using namespace std;
 
 int main(){
 
     int ingreso;
-    cout<<"Usted ha ingresado al sistema de noticias NEWS"<<endl;
-    cout<<"¿Como desea ingresar? Ingrese un numero segun su eleccion"<<endl;
-    cout<<"Autor (1)"<<endl;
-    cout<<"Usuario - Lector (2)"<<endl;
-    cout<<"Deseo salir del programa (3)"<<endl;
-    cin>>ingreso;
-    switch (ingreso) {
-        case 1:
+    do{
+        cout<<"\n\nUsted ha ingresado al sistema de noticias NEWS\n\n";
+        cout<<"Por favor, elija su modo de acceso ingresando el número correspondiente:\n\n";
+        cout<<"1. Autor\n\n";
+        cout<<"2. Usuario - Lector\n\n";
+        cout<<"3. Deseo salir del programa. \n\n";
+        cin>>ingreso;
+        switch (ingreso) {
+            case 1:
+            cout<<"hola";
             //realizar metodos de autor
+
             break;
-        case 2:
+            case 2:
+            cout<<"chau";
             //realizar metodos de usuario
             break;
-        case 3:
+            case 3:
             exit(-1);
-    }
-
+        }
+        if(ingreso!=1 || ingreso != 2){
+            cout<<"\nLa opcion ingresada no es valida. Intente nuevamente.\n\n";
+        }
+    } while (ingreso!=1 || ingreso != 2);
 
     return 0;
 }
