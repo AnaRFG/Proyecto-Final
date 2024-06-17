01d8c546fe2c573566709f453795e5d235507f46
jazminmorenasanchez <2321544@ucc.edu.ar>
2024-06-17 10:13:17 -0300

    modificacion archivos

--- Archivero.cpp
+++ Archivero.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include "Archivero.h"
- using namespace std;
+#include "Persona.h"
+#include "Autor.h"
+#include "Comentario.h"
+#include "Usuario.h"
+#include "Noticia.h"
+using namespace std;
 
-/*
- *
- */
+archivero::archivero(){
+
+}
+
+archivero::archivero(Noticia tit, Noticia desc, Noticia a, Noticia m, Noticia d, Autor autor, Comentarios comentarios){
+
+}
+
+void archivero::leerarchivo(){
+
+}
+void archivero::añadirarchivo(){
+
+}
+
