7c0097901787b38bc8115d70314a4e94db8d193c
jazminmorenasanchez <2321544@ucc.edu.ar>
2024-06-17 10:14:53 -0300

    modificacion archivos

--- Archivero.h
+++ Archivero.h
@@ -1,4 +1,25 @@
 #include <iostream>
-
+#include "Persona.h"
+#include "Autor.h"
+#include "Comentario.h"
+#include "Usuario.h"
+#include "Noticia.h"
 using namespace std;
+
+class archivero : public Noticia, public Comentario {
+    private:
+    Noticia tit;
+    Noticia desc;
+    Noticia a;
+    Noticia m;
+    Noticia d;
+    Autor autor;
+    Comentarios comentarios;
+
+    public:
+    archivero();
+    archivero(Noticia tit, Noticia desc, Noticia a, Noticia m, Noticia d, Autor autor, Comentarios comentarios);
+    void leerarchivo();
+    void añadirarchivo();
+};
 
