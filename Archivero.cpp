6ade0667e7fbbf59544b4571bb35582f62ba0d1c
jazminmorenasanchez <2321544@ucc.edu.ar>
2024-06-17 12:49:51 -0300

    archivero

--- Archivero.cpp
+++ Archivero.cpp
@@ -7,18 +7,3 @@
 #include "Noticia.h"
 using namespace std;
 
-archivero::archivero(){
-
-}
-
-archivero::archivero(Noticia tit, Noticia desc, Noticia a, Noticia m, Noticia d, Autor autor, Comentarios comentarios){
-
-}
-
-void archivero::leerarchivo(){
-
-}
-void archivero::añadirarchivo(){
-
-}
-
